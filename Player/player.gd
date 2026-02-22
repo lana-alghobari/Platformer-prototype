2f2be0b0cef3ed11078081d8178a5647b6b74afc
lana-alghobari <lana.alghobari05@gmail.com>
2026-02-22 19:19:01 +0300

    game over scene

--- Player/player.gd
+++ Player/player.gd
@@ -6,18 +6,18 @@ const JUMP_VELOCITY = -400.0
 @onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 	if (health<=0):
 		anim.play("Death");
+		var tree = get_tree()
 		await anim.animation_finished
-	# Handle jump.
+		tree.change_scene_to_file("res://game_over.tscn")
+		
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		if direction ==1 :
