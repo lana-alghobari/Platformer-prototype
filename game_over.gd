971f0d89945a3fb430949bc3b60c0586946c9fb1
lana-alghobari <lana.alghobari05@gmail.com>
2026-02-28 21:19:47 +0300

    some new details

--- game_over.gd
+++ game_over.gd
@@ -19,3 +19,7 @@ func _on_revive_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_mainmenu_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
